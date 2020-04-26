6545f2790e6983f8069eb58ef84cb105c04c5542
Midii-11 <jak.daxter.midii@gmail.com>
2020-04-26 22:22:28 +0200

    Works independently of working directory

--- Extraction_CSV-Txt.py
+++ Extraction_CSV-Txt.py
@@ -1,22 +1,30 @@
 import pandas as pd
-import numpy as np
 import os
+from pathlib import Path
 
-# Read CSV file
-doc = pd.read_csv("metadata.csv", dtype=str)
+def main():
 
-# Save each abstracts to a .txt file
-for index, row in doc.iterrows():
-    abs = str(row['abstract'])
-    index_str = str(index + 1)
-    path = '/home/midii/Desktop/txt_test/abs_' + index_str +".txt"
-    exist = os.path.isfile(path)
-    if exist == False:
-        with open(path, "w") as text_file:
-            text_file.write(abs)
-        print("saving file n° " + index_str)
-    else:
-        print("File already exists.")
+    # Read CSV file
+    doc = pd.read_csv("metadata.csv", dtype=str)
+
+
+    Path("./Extracted_abstracts").mkdir(parents=True, exist_ok=True)
+
+    # Save each abstracts to a .txt file
+    for index, row in doc.iterrows():
+        abs = str(row['abstract'])
+        index_str = str(index + 1)
+        path = "./Extracted_abstracts/" + "abs_" + index_str +".txt"
+        exist = os.path.isfile(path)
+        if exist == False:
+            with open(path, "w") as text_file:
+                text_file.write(abs)
+            print("saving file n° " + index_str)
+        else:
+            print("File already exists.")
+
+if __name__ == "__main__":
+    main()
 
 
 
